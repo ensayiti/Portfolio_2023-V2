bb7c37074e1c842210f93f7a9f959f2c4395cfdf
samsleepingatparty <itstaurens@gmail.com>
2023-02-07 13:45:53 +0700

    Update Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,6 +7,8 @@ import xquote from "../../Assets/Projects/xQuote.png";
 import xjobdesc from "../../Assets/Projects/xJobDesc.png";
 import xtodo from "../../Assets/Projects/xTodo.png";
 import xweather from "../../Assets/Projects/xWeather.png"
+import xai2 from "../../Assets/Projects/xAI-V2.png";
+import ximage from "../../Assets/Projects/xImage.png";
 
 function Projects() {
   return (
@@ -20,6 +22,17 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={xai2}
+              isBlog={false}
+              title="xAI-V2"
+              description="xAI is a type of artificial intelligence-powered chat interface that allows users to interact with a website or application using natural language. It offers conversational UI for a seamless experience"
+              ghLink="https://github.com/samsleepingatparty/xAI-V2"
+              demoLink="https://ai.sleepingatparty.xyz/"
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={xai}
@@ -72,6 +85,17 @@ function Projects() {
               description="xWeather is a weather web application provides real-time weather information and forecasts for a specified location. It uses data from weather services to show current conditions, temperature, and other relevant weather information."
               ghLink="https://github.com/samsleepingatparty/xWeather"
               demoLink="https://sparkly-strudel-fdd8d1.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ximage}
+              isBlog={false}
+              title="xImage"
+              description="xImage is an artificial intelligence tool that creates unique images based on input parameters, such as keywords, sketches, or styles. It uses machine learning algorithms to produce high-quality, diverse images."
+              ghLink="https://github.com/samsleepingatparty/xImage"
+              demoLink="https://ximage.sleepingatparty/"
             />
           </Col>
         </Row>
